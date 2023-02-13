b905bec94ca4778e6b8c22676dec4d8d9d21b07b
GooEySnow <yanhanphua2@gmail.com>
2023-02-13 22:03:08 +0800

    request backend

---
+++ server/models/Request.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const RequestSchema = new mongoose.Schema({
+  email: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  reason: {
+    type: String,
+  },
+  leaveType: {
+    type: String,
+  },
+  startDate: {
+    type: String,
+  },
+  totalLeaveTaken: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("request", RequestSchema);
