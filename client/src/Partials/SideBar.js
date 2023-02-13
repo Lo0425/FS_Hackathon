d1e401287f0bf9daa988b61e630db6cc8d2f5bc5
Lo0425 <chingclo.lcc@gmail.com>
2023-02-13 22:16:37 +0800

    qweqwe

--- client/src/Partials/SideBar.js
+++ client/src/Partials/SideBar.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 
-const SideBar = ({ logoutHandler }) => {
+
+const SideBar = () => {
   const [open, setOpen] = useState(false);
 
-  
   return (
     // <div className="flex">
     <div
