16e025beae8a719f6b9c81050eafda6ec24d2d5b
Lo0425 <chingclo.lcc@gmail.com>
2023-02-14 11:54:02 +0800

    leave me here

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,11 @@ import Leave from "./Dashboard/Leave";
 import Profile from "./form/Profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { checkAuth } from "./api/users";
 
 function App() {
   const [auth, setAuth] = useState(false);
+  const { user } = checkAuth();
 
   return (
     <>
@@ -25,15 +27,16 @@ function App() {
         pauseOnHover
       />
       <Routes>
-        {auth ? (
-          <Route path="/" element={<Main />} />
+        {user ? (
+          <>
+            <Route path="/" element={<Main />} />
+            <Route path="/leave" element={<Leave />} />
+          </>
         ) : (
           <Route path="/" element={<Navigate to="/login" />} />
         )}
         <Route path="/login" element={<Login setAuth={setAuth} />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/leave" element={<Leave />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
